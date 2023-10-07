999feb0b732d1bbd268e3bb9674bfec87c0c2f5c
Johannes253 <johannesm763@gmail.com>
2023-10-06 20:28:26 -0500

    changed Country Model

--- Models/Country.cs
+++ Models/Country.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesCountry.Models;
 
@@ -7,7 +8,10 @@ public class Country
     public int Id { get; set; }
     public string? Name { get; set; }
     [DataType(DataType.Date)]
+    [Display(Name = "Formation Date")]
     public DateTime FormationDate { get; set; }
+    [Display(Name = "Best Food")]
     public string? BestFood { get; set; }
+    [Display(Name = "Population in Million")]
     public decimal PopulationinMillion { get; set; }
 }
